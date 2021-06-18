49ba1b87cba8c3b0dd7a6157d775ad9d83681e91
Lluna Pellise <68506886+M79CODE@users.noreply.github.com>
2021-06-18 12:49:49 +0200

    Mòdul 4 Nivell 1 Fase 1
    Fase 1 feta

---
+++ M4N1/FASE 1/RestaurantM4N1F1.java
@@ -0,0 +1,36 @@
+package restaurantM4;
+
+import java.util.LinkedHashMap;
+import java.util.Scanner;
+
+public class RestaurantM4N1F1 {
+
+	public static void main(String[] args) {
+	/* NIVELL 1
+	L’exercici consisteix a mostrar per consola una carta d’un restaurant on afegirem diferents plats i després escollirem què volem per menjar. 
+	Un cop fet això s’haurà de calcular el preu del menjar i el programa ens dirà amb quins bitllets hem de pagar.
+	*/
+		
+		/* FASE 1
+		− Creeu una variable int per cada un dels bitllets que existeixen des de 5€ a 500€, haureu de crear un altre variable per guardar el preu total del menjar.
+		− Creeu dos arrays, un on guardarem el menú i un altre on guardarem el preu de cada plat.
+		*/
+		
+		int moneda1;
+		int moneda2;
+		int bitllet5;
+		int bitllet10;
+		int bitllet20;
+		int bitllet50;
+		int bitllet100;
+		int bitllet200;
+		int bitllet500;
+		
+		int preuTotal = 0;
+		
+		String[] plats = new String[10];
+		int[] preus = new int[10];
+		
+	}
+}
+
